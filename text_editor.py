9f48716eb3d4244405f83882fd869388efc0f7b5
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2019-10-01 22:15:36 -0400

    saving progress

--- text_editor.py
+++ text_editor.py
@@ -10,33 +10,74 @@ sg.change_look_and_feel('Black')
 app = shelve.open('settings')
 app['filename'] = None
 
-menu_layout = [['File',['New','Open','Save','Save As','---','Exit']],
-               ['Edit',['Undo','---','Cut','Copy','Paste','Delete','---','Find...','Find Next','Replace...','Go To','---','Select All','Time/Date']],
-               ['Format',['Font','Theme', app['themes']]],
-               ['Run',['Python Shell','Run Module']],
-               ['Help',['View Help','---','About Izzypad 1.0']]]
+def load_settings(window):
+    window['_BODY_'](value=app['body'])
 
-col1 = sg.Column([[sg.Multiline(font=app['font'], key='_BODY_', auto_size_text=True, size=(150,30))]])
-col2 = sg.Column([[sg.Output(size=(150,10), font=('Consolas', 12))]])               
+def save_settings(values):
+    try: # <---------this is not working correctly upon exit
+        app['body'] = values['_BODY_']
+    except:
+        pass
 
-window_layout = [[sg.Menu(menu_layout)],
-          [sg.Text('> New File <', key='_INFO_',font=('Consolas',11), size=(100,1))],
-          [sg.Pane([col1, col2])]]
+#---------MAIN WINDOW LOOP----------#
+def main_loop():
+    menu_layout = [['File',['New','Open','Save','Save As','---','Exit']],
+            ['Edit',['Undo','---','Cut','Copy','Paste','Delete','---','Find...','Find Next','Replace...','Go To','---','Select All','Time/Date']],
+            ['Format',['Font','Theme', app['themes']]],
+            ['Run',['Python Shell','Run Module']],
+            ['Help',['View Help','---','About Izzypad 1.0']]]
 
-window = sg.Window('TextCode Editor', window_layout, resizable=True, margins=(0,0), size=(1200,800), return_keyboard_events=True, finalize=True)
+    col1 = sg.Column([[sg.Multiline(font=app['font'], key='_BODY_', auto_size_text=True, size=(150,30))]])
+    col2 = sg.Column([[sg.Output(size=(150,10), font=('Consolas', 12))]])               
+
+    window_layout = [[sg.Menu(menu_layout)],
+            [sg.Text('> New File <', key='_INFO_',font=('Consolas',11), size=(100,1))],
+            [sg.Pane([col1, col2])]]
+
+    window = sg.Window('TextCode Editor', window_layout, resizable=True, margins=(0,0), size=(1200,800), return_keyboard_events=True, finalize=True)
+    load_settings(window)
+
+    # main window loop
+    while True:
+        event, values = window.read()
+        if event in (None,'Exit'):
+            save_settings(values)
+            break
+        if event == 'Open':
+            open_file(window, event, values)
+        if event in('Save',):
+            save_file(window, values, app['filename'])
+        if event == 'Save As':
+            save_file_as(window, values)
+        if event == 'New':
+            new_file(window)        
+        if event in ('Run Module','F5:116'):
+            run_module(app['filename'], values)
+#        if event == 'Font':
+#            change_font(window)
+        if event == 'Time/Date':
+            timestamp(window, event, values)
+        if event in app['themes']:
+            sg.change_look_and_feel(event)
+            app['theme'] = event
+            save_settings(values)
+            window.close()
+            break
+    return event
+        
 
 #----------FILE OPEN & SAVE FUNCTIONS----------#
 
-def save_file(filename):
+def save_file(window, values, filename):
     ''' save file instantly if already open; otherwise use `save-as` popup '''
     if filename not in (None,''):
         with open(filename,'w') as f:
             f.write(values['_BODY_'])
         window['_INFO_'](value=filename.replace('/',' > '))
     else:
-        save_file_as()
+        save_file_as(window, values)
 
-def save_file_as():
+def save_file_as(window, values):
     ''' save new file or save existing file with another name '''
     app['filename'] = sg.popup_get_file('Save File', save_as=True, no_window=True)
     if app['filename'] not in (None,''):
@@ -44,12 +85,12 @@ def save_file_as():
             f.write(values['_BODY_'])
         window['_INFO_'](value=app['filename'].replace('/',' > '))
 
-def new_file():
+def new_file(window):
     ''' return info bar to default settings '''
     window['_BODY_'](value='')
     window['_INFO_'](value='> New File <')
 
-def open_file():
+def open_file(window, event, values):
     '''open a new file'''
     app['filename'] = sg.popup_get_file('File Name:', title='Open', no_window=True)
     if app['filename'] not in (None,''):
@@ -60,32 +101,31 @@ def open_file():
 
 #----------FORMAT FUNCTIONS----------#
 
-def timestamp():
+def timestamp(window, event, values):
     ''' add the timestamp to the end of the body text '''
     timestamp = datetime.now().strftime("%T %D")
     new_body = values['_BODY_'] + timestamp
     window['_BODY_'](value=new_body)    
 
-font_list = sorted([f for f in font.families() if f[0]!='@'])
-font_sizes = [8,9,10,11,12,14,16,18,20,22,24,26,28,36,48,72]
+#font_list = sorted([f for f in font.families() if f[0]!='@'])
+#font_sizes = [8,9,10,11,12,14,16,18,20,22,24,26,28,36,48,72]
 
-def change_font():
-    '''Change the font in the main multiline element'''
-    global font_name, font_size
-    font_layout = [[sg.Combo(font_list, key='_FONT_', default_value=app['font'][0]), 
-                    sg.Combo(font_sizes, key='_SIZE_', default_value=app['font'][1])],[sg.OK(), sg.Cancel()]]
-    font_window = sg.Window('Font', font_layout, size=(350,80), keep_on_top=True)
-    font_event, font_values = font_window.read()
-    if font_event not in (None,'Exit'):
-        font_name, font_size = (font_values['_FONT_'], font_values['_SIZE_'])
-        app['font'] = (font_name, font_size)
-        window['_BODY_'](font=app['font'])
-        window.refresh()
-    font_window.close()
+# def change_font(window):
+#     '''Change the font in the main multiline element'''
+#     font_layout = [[sg.Combo(font_list, key='_FONT_', default_value=app['font'][0]), 
+#                     sg.Combo(font_sizes, key='_SIZE_', default_value=app['font'][1])],[sg.OK(), sg.Cancel()]]
+#     font_window = sg.Window('Font', font_layout, size=(350,80), keep_on_top=True)
+#     font_event, font_values = font_window.read()
+#     if font_event not in (None,'Exit'):
+#         font_name, font_size = (font_values['_FONT_'], font_values['_SIZE_'])
+#         app['font'] = (font_name, font_size)
+#         window['_BODY_'](font=app['font'])
+#         window.refresh()
+#     font_window.close()
 
 #------------RUN FUNCTIONS-----------#    
 
-def run_module(filename):
+def run_module(filename, values):
     '''the open script'''
     print(f'Running Session >> {filename}')
     print('-'*30)
@@ -94,28 +134,10 @@ def run_module(filename):
     except:
         exec(values['_BODY_']) # need to fix for invalid python code
  
-#---------MAIN WINDOW LOOP----------#
-def main_window():
-    pass
 
 while True:
-    event, values = window.read()
-    if event in (None,'Exit'):
-        app.close()
+    main_event = main_loop()  
+    if main_event is None:
         break
-    if event == 'Open':
-        open_file()
-    if event in('Save',):
-        save_file(app['filename'])
-    if event == 'Save As':
-        save_file_as()
-    if event == 'New':
-        new_file()        
-    if event in ('Run Module','F5:116'):
-        run_module(app['filename'])
-    if event in app['themes']:
-        sg.change_look_and_feel(event)
-    if event == 'Font':
-        change_font()
-    if event == 'Time/Date':
-        timestamp()
+    else:
+        continue
